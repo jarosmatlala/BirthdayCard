7c917da838b7f2d030057005a9cd79a4abaac57a
unknown <matlalaj67@gmail.com>
2024-11-12 13:48:36 +0200

    Created Layout Component

--- rn-birthday/app/_layout.tsx
+++ rn-birthday/app/_layout.tsx
@@ -1,37 +1,116 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React, { useState } from 'react';
+import { Stack } from "expo-router";
+import { View,Text,TextInput, Pressable,StyleSheet,KeyboardAvoidingView} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [readerName, setReaderName] = useState('');
+  const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <SafeAreaView style={styles.container}> 
+  {/* <KeyboardAvoidingView> */}
+
+    <SafeAreaView style={styles.topContainer}>
+   <Text style={styles.appName}>Create Your Card</Text>
+{/* <Pressable onPress={() => {console.log('button pressed')}}>
+  <Text>Submit</Text>
+</Pressable> */}
+    </SafeAreaView>
+
+    <SafeAreaView style={styles.bottomContainer}>
+    <SafeAreaView  style={styles.inputContainer}>
+    <Text style={styles.label}>Full Names</Text>
+    <TextInput 
+    placeholder="Name of Receiver" 
+    style={styles.TextInput}  placeholderTextColor={'#747474'} 
+    value={readerName}
+    onChangeText={setReaderName}/>
+
+<TextInput
+            style={styles.messageInput}
+            value={message}
+            onChangeText={setMessage}
+            multiline
+          />
+
+<Pressable style={styles.uploadButton} onPress={() => {console.log('button pressed')}}>
+  <Text>Submit</Text>
+</Pressable>
+
+    </SafeAreaView>
+    </SafeAreaView>
+    {/* </KeyboardAvoidingView> */}
+    </SafeAreaView>
+
+
+
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#262F5B',
+    paddingVertical: 80,
+    alignItems: 'center',
+  },
+  topContainer: {
+    flex: 1.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  appName: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontFamily: 'berkshire',
+  },
+  bottomContainer:{
+    flex: 2.8,
+    backgroundColor: 'white',
+    padding: 20,
+    minHeight: 420,
+    width: 320,
+    borderRadius: 25, 
+
+   },
+   inputContainer: {
+    flex: 3,
+   },
+   label:{
+    color: '#BDBDBD',
+   },
+   TextInput:{
+      height: 46,
+      borderRadius: 15,
+      borderWidth: 1,
+      backgroundColor: '#1EA0E5',
+      padding: 10,
+      marginVertical: 10,
+   },
+   messageInput: {
+    height: 120,  
+    borderRadius: 15,
+    borderWidth: 1,
+    backgroundColor: '#1EA0E5',
+    padding: 10,
+    marginVertical: 10,
+    textAlignVertical: 'top',
+  },
+  uploadButton: {
+    height: 46,
+    borderRadius: 15,
+    backgroundColor: '#1EA0E5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10, 
+  }
+  
+})
